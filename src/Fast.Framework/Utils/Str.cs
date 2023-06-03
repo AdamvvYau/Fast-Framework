b8aa5a7cd5ed5447ba849ba052fe1dc9ed3da312
China-Mr-zhong <850856667@qq.com>
2023-06-03 17:32:58 +0800

    新增Str工具类
    Signed-off-by: China-Mr-zhong <850856667@qq.com>

---
+++ src/Fast.Framework/Utils/Str.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Fast.Framework.Utils
+{
+
+    /// <summary>
+    /// 字符串工具类
+    /// </summary>
+    public static class Str
+    {
+
+        /// <summary>
+        /// 获取中文字符数量
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static int GetChineseCharCount(string input)
+        {
+            return Regex.Matches(input, "[\u4e00-\u9fa5]|[^\x00-\xff]").Count;
+        }
+    }
+}
